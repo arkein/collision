ebbb548e741cd31d24cb292e3e353de31a4afc4b
Aliaksei.Ziamnovich <aliaksei.ziamnovich@thomsonreuters.com>
2015-08-11 13:57:22 +0300

    initial commit

---
+++ CollisionTest/Interfaces/IDrawingObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollisionTest.Interfaces
+{
+    public interface IDrawingObject
+    {
+        int Left { get; set; }
+        int Width { get; }
+    }
+}
---
+++ CollisionTest/ViewModels/DrawingBoardViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CollisionTest.Interfaces;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace CollisionTest.ViewModels
+{
+    public class DrawingBoardViewModel : ReactiveObject
+    {
+        private readonly ReactiveList<IDrawingObject> _drawingObjects;
+        public DrawingBoardViewModel()
+        {
+            _drawingObjects = new ReactiveList<IDrawingObject> { ChangeTrackingEnabled = true };
+        }
+
+        public IReadOnlyReactiveList<IDrawingObject> Items
+        {
+            get { return _drawingObjects; }
+        }
+
+        public void AddNew(IDrawingObject drawingObject)
+        {
+            drawingObject.Left = _drawingObjects.Sum(x => x.Width);
+            _drawingObjects.Add(drawingObject);
+        }
+    }
+}
